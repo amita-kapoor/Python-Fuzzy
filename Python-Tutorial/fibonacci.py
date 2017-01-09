416197242f38b59226eb545bc0314cb046dead70
Dr Amita Kapoor <kapooramita@gmail.com>
2017-01-09 20:43:29 +0530

    Create fibonacci.py
    Generates Fibonacci sequence less than n.

---
+++ Python-Tutorial/fibonacci.py
@@ -0,0 +1,12 @@
+# Code to generate Fibonacci sequence
+# It uses a function fibonacci to print a fibonacci sequence less than n 
+def fibonacci(n):
+    a, b=0,1
+    while b < n:
+       print b
+       a, b = b, a+b
+
+
+if __name__=='__main__':
+    fibonacci(10)
+    
