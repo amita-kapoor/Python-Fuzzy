6f107f7648731973e5b3a0b28f698fb765dac8ac
Dr Amita Kapoor <kapooramita@gmail.com>
2017-01-12 13:45:57 +0530

    Create __init__.py

---
+++ fuzzyPy/fuzzyPy/__init__.py
@@ -0,0 +1,6 @@
+import numpy as np
+# Implementing different membership functions
+def trimf(x,[a,b,c]):
+    # Implements Triangular Shape Membership function
+    # a: is start of triangle, b: peak, and c the end of triangle
+    f=np.zeros(
