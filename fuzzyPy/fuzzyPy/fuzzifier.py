b4eb21cd83d6e5c92149a1e9eb7acf77daf5274b
Dr Amita Kapoor <kapooramita@gmail.com>
2017-01-16 00:10:34 +0530

    Update fuzzifier.py

--- fuzzyPy/fuzzyPy/fuzzifier.py
+++ fuzzyPy/fuzzyPy/fuzzifier.py
@@ -5,3 +5,7 @@ def trimf(x,a,b,c):
 def trapmf(x,a,b,c,d):
    y=np.amax([np.amin([(x-a)/(b-a), 1, (d-x)/(d-c))],0])
    return y
+              
+def gaussmf(x,s,v):
+   y=np.exp(-(x-c)^2/2*v^2)
+   return y
